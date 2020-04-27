cc82147d3121c307aaa94ace344340dc25878aa2
Julian QG <31473821+JulianQG@users.noreply.github.com>
2020-04-27 17:44:40 +0100

    Update ToDoListController.cs

--- src/ToDoListAPI/Controllers/ToDoListController.cs
+++ src/ToDoListAPI/Controllers/ToDoListController.cs
@@ -32,93 +32,93 @@ namespace ToDoListAPI.Filters
 
 namespace ToDoListAPI.Controllers
 {
-    [HttpOperationExceptionFilterAttribute]
-    public class ToDoListController : ApiController
-    {
-        private string owner = "*";
+   [HttpOperationExceptionFilterAttribute]
+   public class ToDoListController : ApiController
+   {
+      private string owner = "*";
 
-        private static ToDoListDataAPI NewDataAPIClient()
-        {
-            var client = new ToDoListDataAPI(new Uri(ConfigurationManager.AppSettings["toDoListDataAPIURL"]));
-            // Uncomment following line and entire ServicePrincipal.cs file for service principal authentication of calls to ToDoListDataAPI
-            //client.HttpClient.DefaultRequestHeaders.Authorization =
-            //    new AuthenticationHeaderValue("Bearer", ServicePrincipal.GetS2SAccessTokenForProdMSA().AccessToken);
-            return client;
-        }
+      private static ToDoListDataAPI NewDataAPIClient()
+      {
+         var client = new ToDoListDataAPI(new Uri(ConfigurationManager.AppSettings["toDoListDataAPIURL"]));
+         // Uncomment following line and entire ServicePrincipal.cs file for service principal authentication of calls to ToDoListDataAPI
+         //client.HttpClient.DefaultRequestHeaders.Authorization =
+         //    new AuthenticationHeaderValue("Bearer", ServicePrincipal.GetS2SAccessTokenForProdMSA().AccessToken);
+         return client;
+      }
 
-        // GET: api/ToDoItemList
-        public async Task<IEnumerable<ToDoItem>> Get()
-        {
-            // Uncomment following line in each action method for user authentication
-            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
-            using (var client = NewDataAPIClient())
+      // GET: api/ToDoItemList
+      public async Task<IEnumerable<ToDoItem>> Get()
+      {
+         // Uncomment following line in each action method for user authentication
+         //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+         using (var client = NewDataAPIClient())
+         {
+            var results = await client.ToDoList.GetByOwnerAsync(owner);
+            return results.Select(m => new ToDoItem
             {
-                var results = await client.ToDoList.GetByOwnerAsync(owner);
-                return results.Select(m => new ToDoItem
-                {
-                    Description = m.Description,
-                    ID = (int)m.ID,
-                    Owner = m.Owner
-                });
-            }
-        }
+               Description = m.Description,
+               ID = (int)m.ID,
+               Owner = m.Owner
+            });
+         }
+      }
 
-        // GET: api/ToDoItemList/5
-        public async Task<ToDoItem> GetByID(int id)
-        {
-            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
-            using (var client = NewDataAPIClient())
+      // GET: api/ToDoItemList/5
+      public async Task<ToDoItem> GetByID(int id)
+      {
+         //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+         using (var client = NewDataAPIClient())
+         {
+            var result = await client.ToDoList.GetByIdByOwnerAndIdAsync(owner, id);
+            return new ToDoItem
             {
-                var result = await client.ToDoList.GetByIdByOwnerAndIdAsync(owner, id);
-                return new ToDoItem
-                {
-                    Description = result.Description,
-                    ID = (int)result.ID,
-                    Owner = result.Owner
-                };
-            }
-        }
+               Description = result.Description,
+               ID = (int)result.ID,
+               Owner = result.Owner
+            };
+         }
+      }
 
-        // POST: api/ToDoItemList
-        public async Task Post(ToDoItem todo)
-        {
-            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
-            todo.Owner = owner;
-            using (var client = NewDataAPIClient())
+      // POST: api/ToDoItemList
+      public async Task Post(ToDoItem todo)
+      {
+         //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+         todo.Owner = owner;
+         using (var client = NewDataAPIClient())
+         {
+            await client.ToDoList.PostByTodoAsync(new ToDoItem
             {
-                await client.ToDoList.PostByTodoAsync(new ToDoItem
-                {
-                    Description = todo.Description,
-                    ID = todo.ID,
-                    Owner = todo.Owner
-                });
-            }
-        }
+               Description = todo.Description,
+               ID = todo.ID,
+               Owner = todo.Owner
+            });
+         }
+      }
 
-        public async Task Put(ToDoItem todo)
-        {
-            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
-            todo.Owner = owner;
-            using (var client = NewDataAPIClient())
+      public async Task Put(ToDoItem todo)
+      {
+         //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+         todo.Owner = owner;
+         using (var client = NewDataAPIClient())
+         {
+            await client.ToDoList.PutByTodoAsync(new ToDoItem
             {
-                await client.ToDoList.PutByTodoAsync(new ToDoItem
-                {
-                    Description = todo.Description,
-                    ID = todo.ID,
-                    Owner = todo.Owner
-                });
-            }
-        }
+               Description = todo.Description,
+               ID = todo.ID,
+               Owner = todo.Owner
+            });
+         }
+      }
 
-        // DELETE: api/ToDoItemList/5
-        public async Task Delete(int id)
-        {
-            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
-            using (var client = NewDataAPIClient())
-            {
-                await client.ToDoList.DeleteByOwnerAndIdAsync(owner, id);
-            }
-        }
-    }
+      // DELETE: api/ToDoItemList/5
+      public async Task Delete(int id)
+      {
+         //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+         using (var client = NewDataAPIClient())
+         {
+            await client.ToDoList.DeleteByOwnerAndIdAsync(owner, id);
+         }
+      }
+   }
 }
 
